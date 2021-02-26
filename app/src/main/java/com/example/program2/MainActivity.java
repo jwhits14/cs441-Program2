4f8fe4617784ce8bcdf6162bf25b45e079feb5b4
jwhits14 <jakewhit14@gmail.com>
2021-02-26 13:49:59 -0500

    shell of idea created, need to add context to new screens

--- app/src/main/java/com/example/program2/MainActivity.java
+++ app/src/main/java/com/example/program2/MainActivity.java
@@ -2,13 +2,31 @@ package com.example.program2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button grade_button;
+    Button bill_button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        grade_button = findViewById(R.id.grade_option);
+        bill_button = findViewById(R.id.bill_option);
+    }
+
+    public void switchActivityGrades(View view) {
+        Intent act_action = new Intent(this, gradeActivity.class);
+        startActivity(act_action);
+    }
+    public void switchActivityBill(View view) {
+        Intent act_action = new Intent(this, billActivity.class);
+        startActivity(act_action);
     }
 }
