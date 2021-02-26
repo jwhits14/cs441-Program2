ecd1adefa917fc5a95ed87ef380205afb677fe6e
jwhits14 <jakewhit14@gmail.com>
2021-02-26 17:01:31 -0500

    app completed

--- app/src/main/java/com/example/program2/gradeActivity.java
+++ app/src/main/java/com/example/program2/gradeActivity.java
@@ -2,13 +2,24 @@ package com.example.program2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class gradeActivity extends AppCompatActivity {
+
+    Button back_button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_grade);
+
+        back_button = findViewById(R.id.back_button);
+    }
+    public void backToMain(View view) {
+        Intent back = new Intent(this, MainActivity.class);
+        startActivity(back);
     }
 }
